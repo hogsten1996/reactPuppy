1624dc5bdbf25073167c47d1c90d9b644a5803cc
jacobhogsten <hogsten1996@gmail.com>
2023-08-28 14:24:35 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 
+import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
 
+  
+  const [puppies, setPuppies] = useState(puppyList)
+
+
+  console.log(puppies)
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
